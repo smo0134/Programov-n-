b9255f354c86ad7f43cd5c18d3a102c04f711f26
Irena Smolíková <irena.smolikova.st@vsb.cz>
2024-08-01 10:48:23 +0200

    lekce_1_script

---
+++ lekce_1/lekce_1.js
@@ -0,0 +1,28 @@
+/*
+Spustit pomocí příkazového řádku a příkazu node
+*/
+console.log("Hello world!")
+
+/*
+dynamický typovaný jazyk - nemusíme definovat typ proměnné
+string - textové řetězce
+number - čísla(celá, desetinná)
+bigint - velká celá čísla
+boolean - true/false
+object - objekt
+null - hodnota null, když proměnná nemá hodnotu
+undefined - hodnota undefined, když proměnná nemá hodnotu
+*/
+
+// deklarace proměnné pomocí let
+let a //nemusíme rovnou definovat hodnotu
+a = 5
+
+// deklarace proměnné pomocí const
+const b = 10 //musíme rovnou definovat hodnotu, nemůžeme ji měnit
+
+//string
+let text = "Hello world!" //vždy v uvozovkách
+
+//operace s proměnnými
+let c = a + b //sčítání
