86027ae970b87592d2453d1df724582a470d6e5c
Irena Smolíková <irena.smolikova.st@vsb.cz>
2024-09-08 17:18:56 +0200

    První hodina

--- lekce_1/lekce_1.js
+++ lekce_1/lekce_1.js
@@ -1,70 +1,63 @@
-/*
-Spustit pomocí příkazového řádku a příkazu node
-*/
-console.log("Hello world!")
+//vypiš do konzole jakkékoliv číslo
+console.log(10)
 
-/*
-dynamický typovaný jazyk - nemusíme definovat typ proměnné
-string - textové řetězce
-number - čísla(celá, desetinná)
-bigint - velká celá čísla
-boolean - true/false
-object - objekt
-null - hodnota null, když proměnná nemá hodnotu
-undefined - hodnota undefined, když proměnná nemá hodnotu
-*/
+//vypiš do konzole své jméno
+console.log("Irena")
 
-// deklarace proměnné pomocí let
-let a //nemusíme rovnou definovat hodnotu
-a = 5
-console.log(a) //vypíše hodnotu proměnné
+//vytvoř proměnnou x, ulož do ní jakkékoliv číslo a vypiš ji do konzole
+let x = 10
+console.log(x)
 
-// deklarace proměnné pomocí const
-const b = 10 //musíme rovnou definovat hodnotu, nemůžeme ji měnit
+//změn hodnotu proměnné x na jiné číslo a vypiš ji do konzole
+x = 12
+console.log(x)
+
+//vytvoř proměnnou jmeno a prijmeni a ulož do ní své jméno, vypiš ji do konzole
+let jmeno = "Irena"
+let prijmeni = "Smolikova"
+console.log(jmeno)
+console.log(prijmeni)
+
+//vytvoř proměnnou a a b, ulož do nich jakékoli čísla, sečti je, odečti je, vynásob je, vyděl je a využij operaci modulo a vypiš výsledky do konzole
+let a = 10
+let b = 54
+let c = a + b
+let d = a - b
+let e = a * b
+let f = a / b
+let g = a % b
+console.log(a)
 console.log(b)
-
-//string
-let text = "Hello world!" //vždy v uvozovkách
-console.log(text)
-
-//operace s proměnnými
-let c = a + b //sčítání
 console.log(c)
-
-let d = b - a //odečítání
 console.log(d)
-
-let e = a * b //násobení
 console.log(e)
-
-let f = b / a //dělení
 console.log(f)
-
-let g = b % a //modulo - zbytek po dělení
 console.log(g)
 
-//operace s řetězci
-let jmeno = "Irena"
-let prijmeni = "Smolíková"
-let vek = 25
+//sečti proměnné jmeno a prijmeni a vypiš je do konzole
+console.log(jmeno + prijmeni)
+
+//sečti proměnné jmeno a prijmeni a vypiš je do konzole s mezerou
+console.log(jmeno + " " + prijmeni)
+
+//vypište do konzole text Jmenuji se jmeno a prijmeni pomoci vzoru
+console.log(`Jmenuji se ${jmeno} ${prijmeni}.`)
+
+//vytvoř mesto a vek, ulož do nich své město a věk
 let mesto = "Příbor"
+let vek = 20
 
-let celeJmeno = jmeno + " " + prijmeni //spojení řetězců
-let celeJmeno1 = `${jmeno} ${prijmeni}` //spojení řetězců pomocí šablony
-console.log(celeJmeno)
-console.log(celeJmeno1)
+//vytvoř proměnnou celeJmeno, ulož do ní jméno a příjmení oddělené mezerou a vypiš ji do konzole
+let celeJmeno = jmeno + " " + prijmeni
 
-let info = `Jmenuji se ${celeJmeno}, je mi ${vek} let a bydlím ve městě ${mesto}.` //spojení řetězců pomocí šablony
-console.log(info)
+//vypiš do konzole text Jmenuji se celeJmeno, mám vek let a bydlím ve městě mesto
+console.log(`Jmenuji se ${celeJmeno}, mám ${vek} let a bydlím ve městě ${mesto}`)
 
-//boolean
-let pravda = true
-let nepravda = false
-console.log(pravda)
-console.log(nepravda)
+//vytvoř proměnnou vek1 a jeDospely, ulož do ní věk a zjisti zda je jedinec dospěly a vypiš výsledek do konzole
+let vek1 = 23
+let jeDospely = vek1 >= 18
+console.log(jeDospely)
 
-let age = 25
-let isAdult = age >= 18 //porovnání
-console.log(isAdult)
-let isChild = age < 18 //porovnání
-console.log(isChild)
+//vytvoř konstantu y a ulož do ní jakékoli číslo a poté se pokus změnit její hodnotu
+const y = 12
+y = 10
